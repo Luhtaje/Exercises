a8639b9e7bab3e29c8c803b44811051298e8df36
Luhtaje <luhtanen.jere@gmail.com>
2021-10-04 10:46:26 +0300

    Modified redirection after post request, adjusted comments

--- iot/ex2/task3/index.js
+++ iot/ex2/task3/index.js
@@ -16,24 +16,24 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 
 
 app.get('/', (req, res) => {
-    
     updateView(req, res);
 });
 
-
+//Post a new event
 app.post('/events', (req, res) => {
 
     //Create expirydate
     var expiryDate = new Date();
 
     //Ugly check for input validity but rather this than redo whole program.
+    //Prevents input that is in the past
     const eventDate = new Date(req.body.date);
     if((eventDate.getTime() + toMillis(req.body.time) - expiryDate.getTime() - 3*60*60*1000) < 0){
         res.render('events',{error: 'Invalid input. Event was in the past' , events:[]})
         return;
     }
 
-
+    //Expiry date for cookies
     expiryDate.setHours(expiryDate.getHours() + 24);
 
     const cookieOptions = {
@@ -50,12 +50,9 @@ app.post('/events', (req, res) => {
     
     //Creates the cookie from recipies above
     res.cookie(req.body.name, newCookie, cookieOptions);
-    
-    //Ive tried everything to update the view in this post request but I cant get it to work...
-    updateView(req,res);
 });
 
-
+//Clear cookies
 app.get('/reset', (req, res) => {
     Object.keys(req.cookies).forEach(key=>{
         res.clearCookie(key);
@@ -68,7 +65,7 @@ app.listen(port, () => {
     console.log('Listening');
 })
 
-
+//Construcs a string to show time left to event
 function timeLeftString(eventDate,time){
     
     const currDate = new Date;
@@ -93,10 +90,12 @@ function timeLeftString(eventDate,time){
     return ('In ' +event.days + ' days, '+ event.hours+ ' hours, '+ event.minutes + ' minutes ' + event.seconds +' seconds.'); 
 }
 
+//Converts HH:MM time to milliseconds
 function toMillis(time){
     return Number(time.split(':')[0])*60*60*1000+Number(time.split(':')[1])*60*1000;
 }
 
+//Renders the page with currently listed events
 function updateView(req,res){
     const cookiePot = req.cookies;
 
